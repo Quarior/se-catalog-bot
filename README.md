138e9b1dcba0a54701cc1c02b583604ab6965c92
Quarior <quarior14@gmail.com>
2017-04-19 14:59:28 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # se-catalog-bot
 A Discord Bot which catalogues uploaded exported .se files and gives them marks.
+Original by Juxlos (https://github.com/Juxlos/se-catalog-bot)
--- SERegistrationBot.py
+++ SERegistrationBot.py
@@ -1,4 +1,5 @@
 import discord
+import locale
 import asyncio
 import random
 import requests
@@ -11,8 +12,51 @@ from bs4 import BeautifulSoup
 
 # Startup and reference values
 client = discord.Client()
-
-
+#Input
+Register = ';register'
+Score = ';score'
+Systems = ';systems'
+Help = ';help'
+#Locale
+Locale = locale.getdefaultlocale()
+#English default strings
+#Register
+StrCachingFile = 'Caching file...'
+Strbm = ' **Congratulations in uploading your first system ! You earned a 1.5x multiplier for first time !**'
+Stredited_message = '{0} registered to database.\n{1} bodies registered with {2} stars and {3} planets.\n{4} points have been added to {5}\'s account.\nCurrent : {7} points.{6}'
+StrSorry = "Sorry, that system had already been discovered by {0}!"
+StrErrorRegister = 'Register in the format :\n```{0} (link to .sc file) (system name)```'
+StrWait = "{0}, please wait another {1} seconds before registering another system."
+#Score
+StrScore1 = "{0}, you currently have {1} points from {2} systems."
+StrScore0 = "{0}, you have not registered a single system. Use {1} to register an exported system !"
+#Systems
+StrSystems
+StrSystemsMore = 'and {0} more...'
+StrSystems0 = "You have no registered systems."
+#Help
+StrHelp = "Use {0} (link) (system name) to register a system, {1} to see score, and {2} to see registered systems !\nIn order to upload a system, use the \"Export System\" option and upload the .sc file to Discord before getting the link !"
+#Translation
+if Locale[0] == "fr_FR":
+    #French strings
+	#Register
+    StrCachingFile = 'Fichier de mise en cache...'
+	Strbm = ' **Félicitations pour télécharger votre premier système ! Vous avez gagné un multiplicateur 1.5x pour la première fois !**'
+	Stredited_message = '{0} enregistré à la base de donnée.\n{1} astres enregistrés avec {2} étoiles et {3} planètes.\n{4} points ont été ajoutés au compte de {5}.\nActuellement : {7} points.{6}'
+	StrSorry = "Désolé, ce système a déjà été découvert par {0}!"
+	StrErrorRegister = 'Enregistrer dans ce format :\n```{0} (lien au fichier .sc) (nom du système)```'
+	StrWait = "{0}, veuillez attendre {1} secondes avant d'enregistrer un autre système."
+	#Score
+	StrScore1 = "{0}, vous avez actuellement {1} points à partir de {2} systèmes."
+	StrScore0 = "{0}, vous avez actuellement aucun point. Utilisez {1} pour enregistrer un système exporté."
+	#Systems
+	StrSystems = '{0}. {1} - {2} Etoiles et {3} planètes ({4} points)'
+	StrSystemsMore = 'et {0} autres...'
+	StrSystems0 = "Vous n'avez pas enregistrer de systèmes."
+	#Help
+	StrHelp = "Utilisez {0} (lien) (nom du système) pour enregistrer un système, {1} pour voir le score, et {2} pour voir les systèmes enregistrés !\nDans l'ordre pour charger un système, utiliez l'option \"Exporter le système\" et charger le fichier .sc sur Discord avant de donner le lien !"
+	
+#Program
 # All data titles are in proper cases i.e. Data Titles or command e.g. ;asteroid
 
 def check_for_os(string):
@@ -34,7 +78,7 @@ async def on_message(message):
         maid = message.author.id
 
         # Register a system
-        if message.content.startswith(';register'):
+        if message.content.startswith(Register):
             time_list = score['time']
             if message.author.id in list(time_list.keys()):
                 last = time_list[message.author.id]
@@ -49,7 +93,7 @@ async def on_message(message):
                         systemname = file.split('/')[-1].replace('.sc','').replace('_',' ')
                         if systemname not in score['system_list']:
                             opened = requests.get(file).text
-                            messaged = await client.send_message(message.channel, 'Caching file...')
+                            messaged = await client.send_message(message.channel, StrCachingFile)
                             cache_name = 'cache' + '.txt' 
                             with open(cache_name, 'w') as cache_file:
                                 cache_file.write(opened)
@@ -85,7 +129,7 @@ async def on_message(message):
                                 score['users'][maid]['points'] = sys_score
                                 score['users'][maid]['username'] = message.author.name
                                 score['system_list'][systemname] = op_js
-                                bm = ' **Congratulations in uploading your first system! You earned a 1.5x multiplier for first time!**'
+                                bm = "\n" + Strbm
                             cur_sc = score['users'][maid]['points']
                             reg_time = time.time()
                             score['system_list'][systemname]['value'] = int(t_v)
@@ -94,30 +138,30 @@ async def on_message(message):
                             score['system_list'][systemname]['time_discovered'] = reg_time
                             score['system_list'][systemname]['discovered_by'] = message.author.id
                             jsonwrite(score, 'explorer_data.json')
-                            edited_message = '{0} registered to database.\n{1} bodies registered with {2} stars and {3} planets.\n{4} points have been added to {5}\'s account.\nCurrent: {7} points.{6}'.format(nickname, b_c, s_c, p_c, sys_score, message.author.mention, bm, cur_sc)
+                            edited_message = Stredited_message.format(nickname, b_c, s_c, p_c, sys_score, message.author.mention, bm, cur_sc)
                             await client.edit_message(messaged, edited_message)
                         else:
-                            await client.send_message(message.channel, "Sorry, that system had already been discovered by {0}!".format(message.author.display_name))
+                            await client.send_message(message.channel, StrSorry.format(message.author.display_name))
                 else:
-                    await client.send_message(message.channel, 'Register in the format:\n```;register (link to .sc file) (system name)```')
+                    await client.send_message(message.channel, StrErrorRegister.format(Register))
             else:
-                await client.send_message(message.channel, "{0}, please wait another {1} seconds before registering another system.".format(message.author.mention, int(300-time_passed)))
+                await client.send_message(message.channel, StrWait.format(message.author.mention, int(300-time_passed)))
 
         # See balance
-        elif message.content == ';score':
+        elif message.content == Score:
             if maid in list(score['users'].keys()):
-                await client.send_message(message.channel, "{0}, you currently have {1} points from {2} systems.".format(message.author.mention, score['users'][maid]['points'], score['users'][maid]['systems_discovered']))
+                await client.send_message(message.channel, StrScore1.format(message.author.mention, score['users'][maid]['points'], score['users'][maid]['systems_discovered']))
             else:
-                await client.send_message(message.channel, "{0}, you have not registered a single system. Use ;register to register an exported system!".format(message.author.mention)) 
+                await client.send_message(message.channel, StrScore0.format(message.author.mention, Register)) 
         # List systems
-        elif message.content == ';systems':
+        elif message.content == Systems:
             if maid in list(score['users'].keys()):
                 sy = score['system_list']
                 print([k for k in sy])
                 sr = [sy[k] for k in sy if sy[k]['discovered_by'] == maid]
                 sl = []
                 for sdic in sr:
-                    sl.append('{0}. {1} - {2} Stars and {3} planets ({4} points)'.format(sr.index(sdic)+1,sdic['nick'],
+                    sl.append(StrSystems.format(sr.index(sdic)+1,sdic['nick'],
                                                                                         sdic['body_count']['Star'],
                                                                                         sdic['body_count']['Planet'], sdic['value']))     
                 s = '```{0}```'.format('\n'.join(sl))
@@ -128,12 +172,12 @@ async def on_message(message):
                     s = '```{0}```'.format('\n'.join(sl))
                 await client.send_message(message.channel, s)
                 if c != 0:
-                    await client.send_message(message.channel, 'and {0} more...'.format(c))
+                    await client.send_message(message.channel, StrSystemsMore.format(c))
             else:
-                await client.send_message(message.channel, 'You have no registered systems.')
+                await client.send_message(message.channel, StrSystems0)
         
-        elif message.content == ';help':
-            await client.send_message(message.channel, 'Use ;register (link) (system name) to register a system, ;score to see score, and ;system to see registered names!\nIn order to upload a system, use the "Export System" option and upload the .sc file to Discord before getting the link!')
+        elif message.content == Help:
+            await client.send_message(message.channel, StrHelp.format(Register, Score, Systems))
                             
 #Just to know it's running    
 @client.event
@@ -146,5 +190,5 @@ async def on_ready():
         
 
 #Runs the whole thing
-client.run('MjE2NTM1MTc4MDU3NzQ0Mzg0.Cpnf0g.Qw26r3wZ75czjI6tCkoUftIWFUo')
+client.run('MzA0MTg4NDEwMDA2ODYzODcy.C9jcyA.DZ2ELJuTv0NMni-fx13Hvamni9Q')
 
